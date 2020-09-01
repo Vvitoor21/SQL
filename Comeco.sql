9cb4a961e7017dac84c33f62569fa8368eab3c7c
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2020-08-31 21:00:06 -0300

    Update Comeco.sql

--- Comeco.sql
+++ Comeco.sql
@@ -24,13 +24,13 @@ materia_id int,
 );
 
 insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(1,'Vitor Duarte','M',21,1,'São Paulo');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(2,'Eduardo Montenegro','M',20,1,'São Paulo');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(3,'Amanda Pereira','F',21,1,'Rio de Janeiro');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(4,'Amanda Roberta','F',24,1,'Belo Horizonte');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(5,'João Pedro','M',28,1,'Manaus');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(2,'Eduardo Montenegro','M',20,2,'São Paulo');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(3,'Amanda Pereira','F',21,4,'Rio de Janeiro');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(4,'Amanda Roberta','F',24,4,'Belo Horizonte');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(5,'João Pedro','M',28,3,'Manaus');
 insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(6,'Rebecca Oliveira','F',23,1,'João Pessoa');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(7,'Vinicius Gonçalves','M',21,1,'Porto Alegre');
-insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(8,'Sofia Linhares','F',25,1,'Florianópolis');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(7,'Vinicius Gonçalves','M',21,2,'Porto Alegre');
+insert into aluno(id,nome,sexo,idade,id_materia,cidade)values(8,'Sofia Linhares','F',25,3,'Florianópolis');
 
 select * from aluno;
 
@@ -40,4 +40,14 @@ select * from aluno;
 --4 ESTATÍSTICA
 
 insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(1,'Rogerio','M',35,'Programação','Belo Horizonte',1);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(2,'Ricardo','M',40,'Machine Learning','São Paulo',3);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(3,'Kenia','F',37,'Banco de dados','Rio de Janeiro',2);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(4,'Marcia','F',36,'Estatística','São Paulo',4);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(5,'Mariana','F',51,'Programação','São Paulo',1);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(6,'Edson','M',42,'Banco de dados','São Paulo',2);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(7,'Tatiana','F',44,'Estatística','Porto Alegre',4);
+insert into professor(id_prof,nome_prof,sexo_prof,idade,materia_prof,cidade_prof,materia_id)values(8,'Gilson','M',39,'Machine Learning','São Paulo',3);
+
+select * from professor;
+
 
