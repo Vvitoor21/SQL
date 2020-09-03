2929bce3a76586f15bea5a0e4d85ff8837cea472
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-09-03 17:58:07 -0300

    Create CaseBasico.sql

---
+++ CaseBasico.sql
@@ -0,0 +1,9 @@
+Select EmailAddress,
+CASE  WHEN EmailAddress LIKE '%@gmail%' THEN 'GMAIL'
+WHEN EmailAddress LIKE '%@yahoo%' THEN 'YAHOO'
+WHEN EmailAddress LIKE '%@live%' THEN 'MICROSOFT'
+WHEN EmailAddress LIKE '%@outlook%' THEN 'MICROSOFT'
+WHEN EmailAddress LIKE '%@msn%' THEN 'MICROSOFT'
+WHEN EmailAddress LIKE '%@hotmail%' THEN 'MICROSOFT'
+ELSE 'EMAIL NÃO IDENTIFICADO' END AS DOMINIO
+from TABELA_EMAIL
