5a7cfe48068d37fe5f17d6af82d0543643c7c8b4
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-09-15 16:21:08 -0300

    Update UsandoViews.sql

--- UsandoViews.sql
+++ UsandoViews.sql
@@ -1,3 +1,5 @@
+-- Para focalizar, simplificar e personalizar a percepção que cada usuário tem do banco de dados.
+
 create view  Visualizacao as select * from Person.Person
 
 select * from Visualizacao
@@ -5,3 +7,4 @@ select * from Visualizacao
 create view somente_firstname as select FirstName from Person.Person
 
 select * from somente_firstname
+
