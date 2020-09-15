8e0e10afd75a29db61a4b4899db33113a797729b
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-09-15 17:38:19 -0300

    Update UsandoHash.sql

--- UsandoHash.sql
+++ UsandoHash.sql
@@ -13,3 +13,10 @@ select hashbytes('SHA2_256',LastName) as Coluna from Person.Person
 --As aspas simples são obrigatórias. Começando pelo SQL Server 2016 (13.x), todos os algoritmos, exceto SHA2_256 e SHA2_512, foram preteridos.
 
 --Documentação de apoio:' https://docs.microsoft.com/pt-br/sql/t-sql/functions/hashbytes-transact-sql?view=sql-server-ver15 '
+
+create procedure Encripitar as select HASHBYTES('SHA2_256',convert(nvarchar(30),numero)) as numero, numero2 from dbo.test
+
+exec Encripitar
+
+--criando stored procedures podemos melhorarar a performance do banco de dados, e retornar determinados valores para os usuarios.
+--view trabalha  com visualização de dados e o alocamento deles através do select.
