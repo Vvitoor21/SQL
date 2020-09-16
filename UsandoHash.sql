9b63346aa0eb3de3c97572db7a035fd146916567
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-09-16 15:35:26 -0300

    Update UsandoHash.sql

--- UsandoHash.sql
+++ UsandoHash.sql
@@ -5,13 +5,10 @@ select FirstName from Person.Person
 select hashbytes('SHA2_256',LastName) as Coluna from Person.Person
 
 --Algoritimo de Hash: MD2 | MD4 | MD5 | SHA | SHA1 | SHA2_256 | SHA2_512
-
 -- Use nchar quando os tamanhos das entradas de dados de coluna forem consistentes.
 -- Use nvarchar quando os tamanhos das entradas de dados de coluna variarem consideravelmente.
-
 --Identifica o algoritmo de hash a ser usado para aplicar o hash à entrada. Este é um argumento exigido sem padrão.
 --As aspas simples são obrigatórias. Começando pelo SQL Server 2016 (13.x), todos os algoritmos, exceto SHA2_256 e SHA2_512, foram preteridos.
-
 --Documentação de apoio:' https://docs.microsoft.com/pt-br/sql/t-sql/functions/hashbytes-transact-sql?view=sql-server-ver15 '
 
 create procedure Encripitar as select HASHBYTES('SHA2_256',convert(nvarchar(30),numero)) as numero, numero2 from dbo.test
@@ -22,3 +19,5 @@ exec Encripitar
 --view trabalha  com visualização de dados e o alocamento deles através do select.
 
 SELECT CONVERT(NVARCHAR(20), HASHBYTES('SHA2_256', convert(nvarchar(100),PostalCode)),2) as SubscriberKey from Person.Address
+
+select convert(nvarchar(20),hashbytes('SHA2_256',convert(nvarchar100),coluna)),2) as Coluna_Hash from Tabela
