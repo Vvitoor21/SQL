4ad9fc70d30dd707da155da42ba88f9957f40629
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-09-17 17:24:06 -0300

    Create 4_Datas.sql

---
+++ 4_Datas.sql
@@ -0,0 +1,16 @@
+--Trabalhando com Datas
+
+select getdate();
+--Getdate vem como a aestrutura ano,mes,dia - yyyy/mm/dd
+
+select convert(char,getdate(),103) as Data
+select convert(char,getdate(),102) as Data
+
+select day(getdate()) as Dia
+select month(getdate()) as Mes
+select year(getdate()) as Ano
+
+select day('11/12/2020') as Dia
+select month('11/12/2020') as Mes
+select year('11/12/2020') as Ano
+
