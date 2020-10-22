76562664445207d76655483292c25c5f4d31aeac
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2020-10-22 15:06:17 -0300

    Update 4_Datas.sql

--- 4_Datas.sql
+++ 4_Datas.sql
@@ -14,3 +14,19 @@ select day('11/12/2020') as Dia
 select month('11/12/2020') as Mes
 select year('11/12/2020') as Ano
 
+--Datepart
+--the detepart command returns an specific part of a data.
+
+select datepart(day,getdate()) -- returning the current day
+select datepart(year,getdate()) -- returning the current year
+select datepart(month,getdate()) -- returning the current month
+-- and instead the command getdate() we can also put the column about we want to work on.
+                
+-- Dateadd
+--Add a data to the query.
+select dateadd(day,+3,getdate())
+select convert(char,dateadd(day,+3,getdate()),103)
+               
+               
+               
+               
