2e28b2ca3efa7b910f3180904cd4cab413b26732
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-23 14:18:06 -0300

    Update 3_Cases.sql

--- 3_Cases.sql
+++ 3_Cases.sql
@@ -8,7 +8,7 @@ WHEN EmailAddress LIKE '%@msn%' THEN 'MICROSOFT'
 WHEN EmailAddress LIKE '%@hotmail%' THEN 'MICROSOFT'
 ELSE 'EMAIL NÃO IDENTIFICADO' END AS DOMINIO
 from TABELA_EMAIL
---..........................................................................................................................
+
 select x.BusinessEntityID, x.FirstName, x.LastName from  
 ( select distinct p.LastName, p.BusinessEntityID,
 'FirstName' = case when FirstName like 'A%' then 'CLIENTE_XP1' 
@@ -23,4 +23,20 @@ from Person.Person as p
 inner join Person.Address as a
 on p.BusinessEntityID = a.AddressID) as x 
 where x.FirstName != x.LastName order by x.BusinessEntityID
+
+select distinct X.FirstName, X.SOMA from 
+(select convert(varchar,p.ModifiedDate,103) as Data ,p.FirstName, p.MiddleName,
+case when p.FirstName like 'A%' then '000 '+p.FirstName
+     when p.FirstName like 'B%' then '011 '+p.FirstName
+     when p.FirstName like 'C%' then '022 '+p.FirstName
+	 when p.FirstName like 'D%' then '033 '+p.FirstName
+	 when p.FirstName like 'E%' then '044 '+p.FirstName
+else FirstName
+end as SOMA
+from Person.Person as p inner join
+Person.Password as a
+on p.BusinessEntityID = a.BusinessEntityID
+and p.MiddleName is not null) as x
+where X.SOMA != x.FirstName
+order by x.SOMA
 
