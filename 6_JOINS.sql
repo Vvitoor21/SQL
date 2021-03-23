19c2201be029d50dba2c7273746abdd48ee89093
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2021-03-23 14:21:25 -0300

    Update 6_JOINS.sql

--- 6_JOINS.sql
+++ 6_JOINS.sql
@@ -1,1 +1,14 @@
+select * from Person.Person p inner join Person.Password a 
+on p.BusinessEntityID <= 30 
+and  a.BusinessEntityID <=30 
+and a.BusinessEntityID=p.BusinessEntityID
 
+select * from Person.Person p left join Person.Password a 
+on p.BusinessEntityID <= 30 
+and  a.BusinessEntityID <=30 
+and a.BusinessEntityID=p.BusinessEntityID
+
+select * from Person.Person p right join Person.Password a 
+on p.BusinessEntityID <= 30 
+and  a.BusinessEntityID <=30 
+and a.BusinessEntityID=p.BusinessEntityID
