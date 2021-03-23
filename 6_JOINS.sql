eb49c2c5b89c34415539b8aa65179a31caad75da
Vitor <vitor.duartegot@gmail.com>
2021-03-23 15:13:20 -0300

    Update 6_JOINS.sql

--- 6_JOINS.sql
+++ 6_JOINS.sql
@@ -1,3 +1,5 @@
+use AdventureWorks2017
+
 select * from Person.Person p inner join Person.Password a 
 on p.BusinessEntityID <= 30 
 and  a.BusinessEntityID <=30 
@@ -12,3 +14,11 @@ select * from Person.Person p right join Person.Password a
 on p.BusinessEntityID <= 30 
 and  a.BusinessEntityID <=30 
 and a.BusinessEntityID=p.BusinessEntityID
+
+select * from Person.Person
+select * from Person.Password
+
+
+select top 50 inna.BusinessEntityID, p.FirstName, p.MiddleName, p.LastName  from Person.Person p
+inner join ( select BusinessEntityID from Person.Password) inna
+on inna.BusinessEntityID = p.BusinessEntityID
