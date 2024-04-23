e39ebb932d630fdd3e967d5cc8e6716835f15d28
Vitor <64867044+Vvitoor21@users.noreply.github.com>
2024-04-22 22:28:17 -0300

    Update 04_Joins.sql

--- 04_Joins.sql
+++ 04_Joins.sql
@@ -1,5 +1,3 @@
-use AdventureWorks2017
-
 select * from Person.Person p inner join Person.Password a 
 on p.BusinessEntityID <= 30 
 and  a.BusinessEntityID <=30 
@@ -17,7 +15,6 @@ and a.BusinessEntityID=p.BusinessEntityID
 
 select * from Person.Person
 select * from Person.Password
-
 
 select top 50 inna.BusinessEntityID, p.FirstName, p.MiddleName, p.LastName  from Person.Person p
 inner join ( select BusinessEntityID from Person.Password) inna
