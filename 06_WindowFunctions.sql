ea7c99345fcaf3bab56f66135bb96881e5fe7e8f
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-04-27 19:52:48 -0300

    Update 06_WindowFunctions.sql

--- 06_WindowFunctions.sql
+++ 06_WindowFunctions.sql
@@ -4,3 +4,7 @@
 SELECT distinct c.name AS country, m.season, m.home_goal, m.away_goal, 
 avg(m.home_goal + m.away_goal) over() AS overall_avg
 FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id limit 3;
+
+select name, middle_name, family, age,
+avg(age) over() as overall_age
+from families
