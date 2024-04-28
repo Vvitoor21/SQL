685c0c35deb0adf57208abfdb2299207d615a220
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-04-28 18:26:13 -0300

    Update 06_WindowFunctions.sql

--- 06_WindowFunctions.sql
+++ 06_WindowFunctions.sql
@@ -10,5 +10,9 @@ avg(age) over() as overall_age
 from families
 
 select name, age, height, weight,
-rank() (order by height) as ranking_height
+rank() over(order by height) as ranking_height
 from families
+
+select person, age, height,
+rank() over(order by height desc) as ranking_height
+from people
