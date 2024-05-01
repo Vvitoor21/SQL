54aecbc6aeb14951595c77a55aade9311f634b1b
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-05-01 19:03:56 -0300

    Update 06_WindowFunctions.sql

--- 06_WindowFunctions.sql
+++ 06_WindowFunctions.sql
@@ -1,6 +1,3 @@
----How many home_goals and away_goals was stored by country and season and how does it compares
----to the average goals ?
-
 SELECT distinct c.name AS country, m.season, m.home_goal, m.away_goal, 
 avg(m.home_goal + m.away_goal) over() AS overall_avg
 FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id limit 3;
@@ -9,6 +6,10 @@ select name, middle_name, family, age,
 avg(age) over() as overall_age
 from families
 
+select employee, salary,
+sum(salary) over() as overall_salaries
+from staff
+  
 select name, age, height, weight,
 rank() over(order by height) as ranking_height
 from families
