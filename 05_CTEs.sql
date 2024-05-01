847c051d90a31d1134f31f05e0e6ad3acb230e9a
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-05-01 19:04:16 -0300

    Update 05_CTEs.sql

--- 05_CTEs.sql
+++ 05_CTEs.sql
@@ -5,6 +5,6 @@ select p.name, p.middle_name, p.age
 from attributes as p
 
 with name as (
-  select person, age, city
-  from people )
+select person, age, city
+from people )
 select m.person, n.age, n.city from name as n
