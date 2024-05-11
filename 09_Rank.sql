cf52cedb1f19985faa267dec6eeb1952d9af2fb3
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-05-11 19:23:21 -0300

    Update 09_Rank.sql

--- 09_Rank.sql
+++ 09_Rank.sql
@@ -1,1 +1,7 @@
+select name, age, height, weight,
+rank() over(order by height) as ranking_height
+from families
 
+select person, age, height,
+rank() over(order by height desc) as ranking_height
+from people
