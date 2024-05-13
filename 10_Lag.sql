5e80f37a30f51cb82efb8440c63172736a6e1b0e
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-05-13 13:13:01 -0300

    Update 10_Lag.sql

--- 10_Lag.sql
+++ 10_Lag.sql
@@ -4,3 +4,8 @@ Select year, champion,
 lag(champion,1) over(order by year asc) as last_champion
 from champions
 order by year asc
+
+Select year, event champion,
+lag(champion) over(partition event order by event asc, year, asc) as last_event_champion
+from champions
+order by year asc, event asc
