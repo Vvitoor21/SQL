385313070b752c4fe595d72fc4b56b5184fc7062
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-06-16 12:20:05 -0300

    Create Strings_Functions.sql

---
+++ Strings_Functions.sql
@@ -0,0 +1,7 @@
+---concatenating
+
+SELECT first_name || ' ' || last_name || ' <' || email ||'>' AS full_email  FROM customer
+
+---upper(name) VITOR DUARTE
+---lower(name) vitor duarte
+---initcap(name) Vitor Duarte
