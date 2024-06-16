3623e3f3ba986f76323598c1764843b09afe5cbd
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-06-16 13:30:59 -0300

    Update 12_Strings_Functions.sql

--- 12_Strings_Functions.sql
+++ 12_Strings_Functions.sql
@@ -9,3 +9,7 @@ SELECT first_name || ' ' || last_name || ' <' || email ||'>' AS full_email  FROM
 -- Replacing whitespace in the film title with an underscore
   
 SELECT replace(title, ' ', '_') AS title FROM film;
+
+-- Select the first 50 characters of description
+
+SELECT left(description, 50) AS short_desc FROM film AS f; 
