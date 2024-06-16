b9ef8559c815206a8aea21a076dd54984cc21cc4
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-06-16 12:22:56 -0300

    Update 12_Strings_Functions.sql

--- 12_Strings_Functions.sql
+++ 12_Strings_Functions.sql
@@ -5,3 +5,8 @@ SELECT first_name || ' ' || last_name || ' <' || email ||'>' AS full_email  FROM
 ---upper(name) VITOR DUARTE
 ---lower(name) vitor duarte
 ---initcap(name) Vitor Duarte
+
+SELECT 
+  -- Replace whitespace in the film title with an underscore
+  replace(title, ' ', '_') AS title
+FROM film;
