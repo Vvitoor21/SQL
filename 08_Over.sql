f983fd93559c4d7ce0e1944e4f1174305ba48027
Vitor Duarte Bem <64867044+Vvitoor21@users.noreply.github.com>
2024-07-01 12:48:40 -0300

    Update 08_Over.sql

--- 08_Over.sql
+++ 08_Over.sql
@@ -1,24 +1,6 @@
-SELECT distinct c.name AS country, m.season, m.home_goal, m.away_goal, 
-avg(m.home_goal + m.away_goal) over() AS overall_avg
-FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id limit 3;
+/*Simple queries using OVER() function */
 
-select quarto, hospedes,
-avg(hospedes) over() as media_hospedes 
-from pousada
-
-SELECT distinct c.name AS country, m.season,
-sum(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS sum
-FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id
-where c.name = 'Scotland'
-
----Podemos juntas duas clausulas over() para soma e média como no exemplo abaixo
-SELECT distinct c.name AS country, m.season,
-sum(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS sum,
-avg(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS avg
-FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id
-where c.name = 'Scotland'
-
-select name, middle_name, family, age,
+select name, middle_name, family, age, 
 avg(age) over() as overall_age
 from families
 
@@ -38,15 +20,37 @@ select year,month, email_name,
 avg(send) over(partition by year,month,email_name) as avg_send
 from tracking_emails
 
+select employee,
+sum(salaries) over(partition by employee) as group_em
+from staff
+
+select quarto, hospedes,
+avg(hospedes) over() as media_hospedes_total
+from pousada
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/ 
+  
 select year,  month, email_name,
 avg(send) over(partition by year,month,email_name) as avg_send
 sum(send) over(partition by year,month,email_name) as sum_send
 count(send) over(partition by year,month,email_name) as count_send ---Counting the lines
 from tracking_emails
 
-select employee,
-sum(salaries) over(partition by employee) as group_em
-from staff
+SELECT distinct c.name AS country, m.season, m.home_goal, m.away_goal, 
+avg(m.home_goal + m.away_goal) over() AS overall_avg
+FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id limit 3;
+
+SELECT distinct c.name AS country, m.season,
+sum(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS sum
+FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id
+where c.name = 'Scotland'
+
+---Podemos juntas duas clausulas over() para soma e média como no exemplo abaixo
+SELECT distinct c.name AS country, m.season,
+sum(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS sum,
+avg(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS avg
+FROM soccer.match AS m LEFT JOIN soccer.country AS c ON m.country_id = c.id
+where c.name = 'Scotland'
 
 SELECT distinct c.name AS country, m.season,
 sum(m.home_goal + m.away_goal) over(partition by c.name, m.season) AS sum
